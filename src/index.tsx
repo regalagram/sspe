dc0bff42442ce0f79fddacc31e1a3505c3cd394a
Ernesto Krsulovic <ernesto.krsulovic@gmail.com>
2025-07-07 12:53:20 -0400

    refactor: Remove global debugging references for toolModeManager and shapeManager

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { SvgEditor } from './core/SvgEditor';
 import './styles/editor.css';
-import { toolModeManager } from './managers/ToolModeManager';
-import { shapeManager } from './plugins/shapes/ShapeManager';
-
-// Hacer toolModeManager y shapeManager disponibles globalmente para debugging
-(window as any).toolModeManager = toolModeManager;
-(window as any).shapeManager = shapeManager;
 
 const App = () => {
   return <SvgEditor />;
